fa18018212fea3de191f976985fb4c103ef4de85
krunalkhairanar-bacancy <krunal.khairanar@bacancy.us>
2026-03-18 18:06:14 +0530

    book list html created

--- Sessions/08_book_store_application/src/app/book/components/book-list/book-list.ts
+++ Sessions/08_book_store_application/src/app/book/components/book-list/book-list.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IBookDetails } from '../../../types/book-type';
+import { BookItem } from "../book-item/book-item";
 
 @Component({
   selector: 'app-book-list',
-  imports: [],
+  imports: [BookItem],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
 export class BookList {
+  @Input() bookList : IBookDetails[] = [{
+    id: 0,
+    title: '',
+    author: '',
+    description: '',
+    price: 0,
+    imgSource: ''
+  }];
 
 }
