37ea4a354f3e31c7fe6b9a5d1ba29a4ff2b684bf
krunalkhairanar-bacancy <krunal.khairanar@bacancy.us>
2026-03-18 17:17:30 +0530

    components : book-item, book-list, book-container created

---
+++ Sessions/08_book_store_application/src/app/pages/book-container/book-container.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-container',
+  imports: [],
+  templateUrl: './book-container.html',
+  styleUrl: './book-container.css',
+})
+export class BookContainer {
+
+}
