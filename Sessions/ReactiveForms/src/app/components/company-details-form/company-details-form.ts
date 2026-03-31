1590725cddf8310cc51e83d21e9cf74afe87ac36
krunalkhairanar-bacancy <krunal.khairanar@bacancy.us>
2026-03-31 09:30:14 +0530

    add and remove functions adde

--- Sessions/ReactiveForms/src/app/components/company-details-form/company-details-form.ts
+++ Sessions/ReactiveForms/src/app/components/company-details-form/company-details-form.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-company-details-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './company-details-form.html',
   styleUrl: './company-details-form.css',
 })
 export class CompanyDetailsForm {
+  projectDetails = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(5)]),
+    description: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(10),
+    ]),
+    startDate: new FormControl('', [Validators.required]),
+    endDate: new FormControl('', [Validators.required]),
+  });
+  companyForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(5)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    website: new FormControl('', [Validators.required]),
+    phone: new FormControl('', [Validators.required]),
+    projects: new FormArray([this.projectDetails]),
+  });
 
+  onSubmit() {
+    console.log('calling');
+    console.log(this.companyForm);
+  }
+
+  get projects() {
+    return this.companyForm.get('projects') as FormArray;
+  }
+  addProject() {
+    this.projects.push(this.projectDetails);
+  }
+  removeProject(index: number) {
+    this.projects.removeAt(index);
+  }
 }
--- Sessions/ReactiveForms/src/app/pages/home/home.ts
+++ Sessions/ReactiveForms/src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CompanyDetailsForm } from "../../components/company-details-form/company-details-form";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CompanyDetailsForm],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
