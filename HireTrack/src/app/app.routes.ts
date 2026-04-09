b489b85a10c476f92e9f1212ccd377a32381ed6d
krunalkhairanar-bacancy <krunal.khairanar@bacancy.us>
2026-04-09 13:42:32 +0530

    auth service, auth guard added and routes configured

--- HireTrack/src/app/app.routes.ts
+++ HireTrack/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/dashboard/dashboard';
+import { authGuard } from './core/guards/auth-guard';
+import { Login } from './features/login/login';
+import { JobDetail } from './features/job-detail/job-detail';
+import { AddJob } from './features/add-job/add-job';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'login',
+    component: Login,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'jobs/:id',
+    component: JobDetail,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'add',
+    component: AddJob,
+    canActivate: [authGuard],
+  },
+];
--- HireTrack/src/app/core/services/auth-service.ts
+++ HireTrack/src/app/core/services/auth-service.ts
@@ -1,8 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  
+  constructor(private http: HttpClient) {}
+
+  private keys = {
+    accessToken: 'access_token',
+    user: 'user',
+  };
+
+  login(email: string, password: string) {
+    return this.http
+      .post(`${environment.supabaseUrl}/auth/v1/token?grant_type=password`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((res: any) => {
+          localStorage.setItem(this.keys.accessToken, res.access_token);
+          localStorage.setItem(this.keys.user, res.user);
+        }),
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.keys.accessToken);
+    localStorage.removeItem(this.keys.user);
+  }
+
+  getToken() {
+    return localStorage.getItem(this.keys.accessToken);
+  }
+
+  isLoggedIn() {
+    return this.getToken() !== null;
+  }
 }
