71c7c4b76cda03172ff0c5d42ed2696a3dfeaf35
krunalkhairanar-bacancy <krunal.khairanar@bacancy.us>
2026-04-10 16:24:25 +0530

    auth header added in supabase interceptor

--- HireTrack/src/app/core/interceptor/supabase-key-interceptor.ts
+++ HireTrack/src/app/core/interceptor/supabase-key-interceptor.ts
@@ -1,12 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth-service';
 
 export const supabaseKeyInterceptor: HttpInterceptorFn = (req, next) => {
   const supabaseKey = environment.supabaseKey;
+  const authService = inject(AuthService);
 
   const newReq = req.clone({
     setHeaders: {
       apiKey: supabaseKey,
+      Authorization: `Bearer ${supabaseKey}`,
     },
   });
 
