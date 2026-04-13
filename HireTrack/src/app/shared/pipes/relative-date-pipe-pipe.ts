65345c914c915765af2ae57bf58fe4a3a36c694b
krunalkhairanar-bacancy <krunal.khairanar@bacancy.us>
2026-04-13 10:18:26 +0530

    relative date pipe created

---
+++ HireTrack/src/app/shared/pipes/relative-date-pipe-pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'relativeDatePipe',
+})
+export class RelativeDatePipePipe implements PipeTransform {
+  transform(value: Date, ...args: unknown[]): string {
+    const now = new Date();
+    const diff = now.getTime() - new Date(value).getTime();
+    const diffInDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    if (diffInDays === 0) {
+      return 'Today';
+    } else if (diffInDays === 1) {
+      return 'Yesterday';
+    } else {
+      return `${diffInDays} days ago`;
+    }
+  }
+}
